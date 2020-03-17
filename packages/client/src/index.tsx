2dff47737e59e641f14edb40e3f70e62766d74e4
Sachin Ahya <sachinahya3@gmail.com>
2020-03-17 22:56:12 +0000

    Remove CRA service worker

--- packages/client/src/index.tsx
+++ packages/client/src/index.tsx
@@ -13,7 +13,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { baseTheme } from 'styles/themes';
 import App from './App';
-import { unregister } from './serviceWorker';
 
 const client = new ApolloClient({
   link: ApolloLink.from([
@@ -42,8 +41,3 @@ ReactDOM.render(
   </ApolloProvider>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-unregister();
