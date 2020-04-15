feb93ebacd166cfd7f3f2c2c121111978d5c1471
Sachin Ahya <sachinahya3@gmail.com>
2020-04-15 21:05:46 +0100

    Use schema from file

--- packages/client/apollo.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  client: {
-    includes: ['src/**/!(*.gql).{gql,ts,tsx}'],
-    service: {
-      name: 'recipe-app',
-      url: 'http://localhost:4000/graphql',
-    },
-  },
-};
